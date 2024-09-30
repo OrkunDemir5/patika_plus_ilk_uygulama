cda8499bb7c040b5591e21cb6b470d1ec5850909
OrkunDemir5 <orkun.d1999@gmail.com>
2024-09-30 18:54:51 +0300

    ilk_uygulama

---
+++ Patika+/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Orkun Demir");
+            Console.WriteLine("Patika'nin umzan kadrosu ve tecrübeli egitmenleri sayesinde" +
+                " yazilim dünyasina hizli bir giriş ve kendimi daha iyi geliştireceğim için bu bootcampteyim!");
+        }
+
+    }
+}
